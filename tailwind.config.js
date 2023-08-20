4dd777fbd6709df5510f40c4d8d74b12e78e3f73
javad7721 <m.javad7721@gmail.com>
2023-08-20 15:21:38 +0330

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    fontFamily: {
+      "iran-sans": "IranSans",
+      "iran-sans-medium": "IranSansMedium",
+    },  
+    screens: {
+      xs: "200px",
+      sm: "600px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1400px",
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
