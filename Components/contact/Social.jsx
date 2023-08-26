d05fa449e3e064aaad12111070bb2cc3f7802373
javad7721 <m.javad7721@gmail.com>
2023-08-26 14:51:21 +0330

    feat:icons social

---
+++ Components/contact/Social.jsx
@@ -0,0 +1,30 @@
+import { faGithub, faLinkedin, faTelegram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+export const Social = ({ data }) => {
+  const info = data.information;
+
+  return (
+    <div className="flex justify-center my-12">
+      {info.map(({ social }) => (
+        social.map(({ linkedin,telegeram,github,whatsapp }) => (
+          <div className="w-1/4 text-4xl flex justify-evenly">
+          <Link target="_blank" key={linkedin} href={linkedin}>
+                <FontAwesomeIcon icon={faLinkedin} className="hover:text-blue-500"/>
+            </Link>
+            <Link target="_blank" key={github} href={github}>
+                <FontAwesomeIcon icon={faGithub} className="hover:text-black hover:bg-white" />
+            </Link>
+            <Link target="_blank" key={telegeram} href={telegeram}>
+                <FontAwesomeIcon icon={faTelegram} className="hover:text-blue-400"/>
+            </Link>
+            <Link target="_blank" key={whatsapp} href={whatsapp}>
+                <FontAwesomeIcon icon={faWhatsapp} className="hover:text-green-700"/>
+            </Link>
+                </div>
+        ))
+      ))}
+    </div>
+  );
+};
---
+++ app/Loading.js
@@ -0,0 +1,8 @@
+export const Loading = () => {
+    return (
+      <div>
+        <p className="text-white my-6 px-[10px] lg:px-[30px]">در حال بارگذاری داده‌ها...</p>
+      </div>
+    );
+  };
+  
