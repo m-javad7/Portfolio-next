d627983aee83731b9ef4b92a137e65fff84bda4a
javad7721 <m.javad7721@gmail.com>
2023-08-26 14:49:43 +0330

    feat:image landing

---
+++ Components/DarkMode.jsx
@@ -0,0 +1,25 @@
+"use client"
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
+import { FaMoon, FaSun } from 'react-icons/fa'
+
+const DarkModeButton = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+  return (
+    <button className=' right-4 bottom-10 text-2xl fixed' onClick={e => theme === 'dark' ? setTheme('light') : setTheme('dark')}>
+        {theme === 'dark' ? <FaMoon /> : <FaSun />}
+    </button>
+  )
+}
+
+export default DarkModeButton
---
+++ Components/mainpage/Shape.jsx
@@ -0,0 +1,7 @@
+import "@/app/globals.css"
+
+export const Shape = () =>{
+    return(
+        <div className="xs:hidden md:block transition-all ease-in-out duration-300 w-[45%] h-full bg-green-700 absolute left-0 top-0 -z-[1] shape"></div>
+    )
+}
