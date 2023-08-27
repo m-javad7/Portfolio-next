d1e18ac668a1043a9e0cdac3f2b372cb6420e2dc
javad7721 <m.javad7721@gmail.com>
2023-08-27 17:37:42 +0330

    Initial commit from Create Next App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx ,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
-  purge: [
-    "./pages/**/*.{js, ts, jsx, tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
+
   darkMode: "class",
   theme: {
     fontFamily: {
       "iran-sans": "IranSans",
       "iran-sans-medium": "IranSansMedium",
-    },  
+    },
     screens: {
       xs: "200px",
       sm: "600px",
