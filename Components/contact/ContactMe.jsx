91d17cac08cb38fdb77b934e43f016b24dcb0b84
M.Javad Darezereshki <128983576+m-javad7@users.noreply.github.com>
2023-08-27 17:51:34 +0330

    Delete Components directory

--- Components/contact/ContactMe.jsx
+++
@@ -1,8 +0,0 @@
-
-export const ContactMe = ()=>{
-    return(
-        <div>
-            <h1 className="text-center mt-11 text-5xl md:text-6xl">راه ارتباطی با <span className="text-green-600">من</span></h1>
-        </div>
-    )
-}
